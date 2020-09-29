52829be39665070a986af78c2fc705445ccf6e25
Fábio Filho <fabiopmfilho@outlook.com>
2020-09-29 17:34:30 -0300

    att

--- desafio3/src/App.js
+++ desafio3/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import style from './components/style.css';
+
+const hello = "hello world"
 
 function App() {
+  const hello = "hello world"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Desafio 3</h1>
+      <p>{HelloWorld()}</p>
     </div>
   );
 }
+
+function HelloWorld(){
+  const txt = [
+    hello.toUpperCase() + " " + dataAtual(),
+    hello.toLowerCase() + " "  + dataAtual(),
+    hello.split("").join(" ") + " "  + dataAtual(),
+    hello.split("") + " " + dataAtual(),
+    hello.toLowerCase().split('').join('*') + dataAtual(),
+    hello.split("").reverse().join("") + " "  + dataAtual(),
+    hello.split(" ").reverse().join(" ") + " "  + dataAtual(),
+    hello.split("").reverse().join("!") + " "  + dataAtual(),
+    hello.split("").join(' ,') + " " + dataAtual(),
+    hello.split("").reverse().join("-") + " "  + dataAtual(),
+  ];
+return txt.map((txt) => <li>{txt}</li>)
+}
+
+function dataAtual() {
+  let data = new Date().toLocaleString();
+  return data;
+}
+
 
 export default App;
